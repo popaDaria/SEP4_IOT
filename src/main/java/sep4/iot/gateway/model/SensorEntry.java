8ff7dc6dd99c2b63779e32fc5ff2b8ae3fa855e0
popad <popad@DESKTOP-T4OR9QR>
2021-04-16 18:30:00 +0200

    commit - lorawan client class added

--- src/main/java/sep4/iot/gateway/model/SensorEntry.java
+++ src/main/java/sep4/iot/gateway/model/SensorEntry.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class SensorEntry {
     private int entry_key;
     private int user_key;
+    private String hweui;
     private Date entry_time;
 
     private float air_temperature;
@@ -20,9 +21,10 @@ public class SensorEntry {
     public SensorEntry() {
     }
 
-    public SensorEntry(int entry_key, int user_key, Date entry_time, float air_temperature, float air_humidity, int air_co2, float light_level, float desired_air_temperature, float desired_air_humidity, int desired_air_co2, float desired_light_level) {
+    public SensorEntry(int entry_key, String hweui, int user_key, Date entry_time, float air_temperature, float air_humidity, int air_co2, float light_level, float desired_air_temperature, float desired_air_humidity, int desired_air_co2, float desired_light_level) {
         this.entry_key = entry_key;
         this.user_key = user_key;
+        this.hweui = hweui;
         this.entry_time = entry_time;
         this.air_temperature = air_temperature;
         this.air_humidity = air_humidity;
@@ -34,9 +36,10 @@ public class SensorEntry {
         this.desired_light_level = desired_light_level;
     }
 
-    public SensorEntry(int entry_key, int user_key, Date entry_time, float air_temperature, float air_humidity, int air_co2, float light_level) {
+    public SensorEntry(int entry_key, int user_key, String hweui, Date entry_time, float air_temperature, float air_humidity, int air_co2, float light_level) {
         this.entry_key = entry_key;
         this.user_key = user_key;
+        this.hweui = hweui;
         this.entry_time = entry_time;
         this.air_temperature = air_temperature;
         this.air_humidity = air_humidity;
@@ -48,6 +51,14 @@ public class SensorEntry {
         desired_air_humidity=-1;
         desired_light_level=-1;
         desired_air_temperature=-1;
+    }
+
+    public String getHweui() {
+        return hweui;
+    }
+
+    public void setHweui(String hweui) {
+        this.hweui = hweui;
     }
 
     public float getDesired_air_temperature() {
---
+++ src/main/java/sep4/iot/gateway/webSocket/WebSocketClient.java
@@ -0,0 +1,87 @@
+package sep4.iot.gateway.webSocket;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.WebSocket;
+import java.nio.ByteBuffer;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.SynchronousQueue;
+
+public class WebSocketClient implements WebSocket.Listener {
+
+    private WebSocket server = null;
+    private Queue<String> dataQueue;
+
+    // Send down-link message to device
+    // Must be in Json format according to https://github.com/ihavn/IoT_Semester_project/blob/master/LORA_NETWORK_SERVER.md
+    public void sendDownLink(String jsonTelegram) {
+        server.sendText(jsonTelegram,true);
+    }
+
+    public String getUpLink(){
+        return dataQueue.poll();
+    }
+
+    // E.g. url: "wss://iotnet.teracom.dk/app?token=??????????????????????????????????????????????="
+    // Substitute ????????????????? with the token you have been given
+    public WebSocketClient(String url) {
+        dataQueue = new SynchronousQueue<>();
+        HttpClient client = HttpClient.newHttpClient();
+        CompletableFuture<WebSocket> ws = client.newWebSocketBuilder()
+                .buildAsync(URI.create(url), this);
+        server = ws.join();
+    }
+
+    //onOpen()
+    public void onOpen(WebSocket webSocket) {
+        // This WebSocket will invoke onText, onBinary, onPing, onPong or onClose methods on the associated listener (i.e. receive methods) up to n more times
+        webSocket.request(1);
+        System.out.println("WebSocket Listener has been opened for requests.");
+    }
+
+    //onError()
+    public void onError​(WebSocket webSocket, Throwable error) {
+        System.out.println("A " + error.getCause() + " exception was thrown.");
+        System.out.println("Message: " + error.getLocalizedMessage());
+        webSocket.abort();
+    };
+    //onClose()
+    public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
+        System.out.println("WebSocket closed!");
+        System.out.println("Status:" + statusCode + " Reason: " + reason);
+        return new CompletableFuture().completedFuture("onClose() completed.").thenAccept(System.out::println);
+    };
+    //onPing()
+    public CompletionStage<?> onPing​(WebSocket webSocket, ByteBuffer message) {
+        webSocket.request(1);
+        System.out.println("Ping: Client ---> Server");
+        System.out.println(message.asCharBuffer().toString());
+        return new CompletableFuture().completedFuture("Ping completed.").thenAccept(System.out::println);
+    };
+    //onPong()
+    public CompletionStage<?> onPong​(WebSocket webSocket, ByteBuffer message) {
+        webSocket.request(1);
+        System.out.println("Pong: Client ---> Server");
+        System.out.println(message.asCharBuffer().toString());
+        return new CompletableFuture().completedFuture("Pong completed.").thenAccept(System.out::println);
+    };
+    //onText()
+    public CompletionStage<?> onText​(WebSocket webSocket, CharSequence data, boolean last) {
+        String indented = null;
+        try {
+            indented = (new JSONObject(data.toString())).toString(4);
+            dataQueue.add(data.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        System.out.println(indented);
+        webSocket.request(1);
+        return new CompletableFuture().completedFuture("onText() completed.").thenAccept(System.out::println);
+    };
+}
