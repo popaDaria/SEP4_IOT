6525bd7fcb0d133ee126602fdb4def84b7d046e6
popad <popad@DESKTOP-T4OR9QR>
2021-04-16 13:34:23 +0200

    initial commit

---
+++ src/main/java/sep4/iot/gateway/controller/HardwareController.java
@@ -0,0 +1,12 @@
+package sep4.iot.gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hardware")
+public class HardwareController {
+
+
+
+}
